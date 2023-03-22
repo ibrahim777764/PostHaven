70a35a8a1fed27e28af0ac9c697908e93f57778f
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-03-23 02:07:31 +0500

    now you ca create new post and edit it

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# models.py
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    my_field = models.CharField(max_length=100, default='')  # add this line to define a new field with a default value
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+
