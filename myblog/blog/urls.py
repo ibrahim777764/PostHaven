fb37500b8ba15c83ed7547e2c16a7bc2a9eff281
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-03-24 17:55:39 +0500

    now user can edit there post

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -11,10 +11,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+        path('post/<int:pk>/update/', views.post_update, name='post_update'),
+]
 
-
-] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 urlpatterns += staticfiles_urlpatterns()
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .forms import PostForm  # import PostForm here
 from django.shortcuts import render, redirect  # import redirect function
 from django.utils import timezone
+from django.urls import reverse
 
 
 
@@ -14,8 +15,11 @@ def post_list(request):
 
 
 def post_detail(request, pk):
+    print(pk)  # Add this line to check the pk value
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'pk': pk})
+
+
 
 def post_new(request):
     if request.method == "POST":
@@ -32,15 +36,37 @@ def post_new(request):
     return render(request, 'blog/post_new.html', {'form': form})
 
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})       
+    return render(request, 'blog/post_edit.html', {'form': form})
+  
+
+
+def post_update(request, pk):
+    print("PK value:", pk)  # Add this line to check the pk value
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog/post_edit.html', {'form': form, 'post': post})
+
+
+
+
+def my_view(request):
+    post = Post.objects.get(pk=16)
+    url = reverse('post_update', kwargs={'pk': post.pk})
+    # ...
+
