e04842fcd4506cb8b32a548464f9a2eb9b25f0e0
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-04-03 03:45:50 +0500

    url path change

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/<int:pk>/update/', views.post_update, name='post_update'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
